b0e939fb518e130c174ff6246db90a00d989aa12
Rurien <a01092603909@gmail.com>
2025-12-12 16:36:31 +0900

    Add newline on build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,10 +30,14 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("tools.jackson.module:jackson-module-kotlin")
+
   developmentOnly("org.springframework.boot:spring-boot-devtools")
+
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
   testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
