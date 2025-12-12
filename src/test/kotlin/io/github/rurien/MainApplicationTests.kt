2098050e079a90956b3644001cad50e3c457c579
Rurien <a01092603909@gmail.com>
2025-12-12 14:13:06 +0900

    Initialize kotlin spring boot project

---
+++ src/test/kotlin/io/github/rurien/MainApplicationTests.kt
@@ -0,0 +1,11 @@
+package io.github.rurien
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainApplicationTests {
+  @Test
+  fun contextLoads() {
+  }
+}
