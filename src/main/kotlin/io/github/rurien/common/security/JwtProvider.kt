2f97520306a89aa2fa5d8fa2427098b241dee1cf
Rurien <a01092603909@gmail.com>
2025-12-14 15:26:47 +0900

    Add JWT provider

---
+++ src/main/kotlin/io/github/rurien/common/security/JwtProvider.kt
@@ -0,0 +1,86 @@
+package io.github.rurien.common.security
+
+import io.github.rurien.common.property.JwtProperties
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import java.nio.charset.StandardCharsets
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.ZoneId
+import javax.crypto.SecretKey
+
+class JwtProvider(
+  private val jwtProperties: JwtProperties,
+) {
+  companion object {
+    const val CLAIMS_TYPE = "CLIENT"
+    const val CLAIMS_ROLE = "PUBLIC"
+  }
+
+  private val signingKey: SecretKey =
+    Keys.hmacShaKeyFor(jwtProperties.secret.toByteArray(StandardCharsets.UTF_8))
+
+  private val accessTokenExpiration: Duration = jwtProperties.accessToken.expiration
+
+  fun create(): String {
+    val now = LocalDateTime.now()
+    val issuedAt =
+      now
+        .atZone(ZoneId.systemDefault())
+        .toInstant()
+        .epochSecond
+    val expirationTime =
+      now
+        .plus(accessTokenExpiration)
+        .atZone(ZoneId.systemDefault())
+        .toInstant()
+        .epochSecond
+
+    return Jwts
+      .builder()
+      .subject("client")
+      .claim("type", CLAIMS_TYPE)
+      .claim("role", CLAIMS_ROLE)
+      .claim("iat", issuedAt)
+      .claim("exp", expirationTime)
+      .signWith(signingKey)
+      .compact()
+  }
+
+  fun validate(jwt: String): Boolean =
+    runCatching {
+      Jwts
+        .parser()
+        .verifyWith(signingKey)
+        .build()
+        .parseSignedClaims(jwt)
+
+      true
+    }.getOrElse { false }
+
+  fun getAuthentication(jwt: String): Authentication {
+    val claims =
+      Jwts
+        .parser()
+        .verifyWith(signingKey)
+        .build()
+        .parseSignedClaims(jwt)
+        .payload
+
+    require(claims["type"] == CLAIMS_TYPE)
+
+    val principal =
+      ClientPrincipal(
+        clientId = claims.subject,
+      )
+
+    return UsernamePasswordAuthenticationToken(
+      principal,
+      null,
+      listOf(SimpleGrantedAuthority("ROLE_PUBLIC")),
+    )
+  }
+}
