50b40694a3900bb4a3946a0e400b152d39fc9cc9
Rurien <a01092603909@gmail.com>
2025-12-16 12:59:28 +0900

    Move dependenciest to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,23 @@
 plugins {
-  kotlin("jvm") version "2.2.21"
-  kotlin("plugin.spring") version "2.2.21"
-  id("org.springframework.boot") version "4.0.0"
-  id("io.spring.dependency-management") version "1.1.7"
-  id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.kotlin.spring)
+  alias(libs.plugins.spring.boot)
+  alias(libs.plugins.spring.dependency.management)
+  alias(libs.plugins.ktlint)
 }
 
-group = "org.example"
-version = "0.0.1-SNAPSHOT"
+group = "io.github.rurien"
+version = "0.0.0.1"
 description = "kotlin-spring"
 
 java {
   toolchain {
-    languageVersion = JavaLanguageVersion.of(21)
+    languageVersion =
+      JavaLanguageVersion.of(
+        libs.versions.java
+          .get()
+          .toInt(),
+      )
   }
 }
 
@@ -27,24 +32,22 @@ repositories {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-webmvc")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("tools.jackson.module:jackson-module-kotlin")
-  implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+  implementation(libs.bundles.spring.boot)
+  implementation(libs.bundles.kotlin.core)
+  implementation(libs.bundles.serialization)
+  implementation(libs.bundles.logging)
 
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  annotationProcessor(libs.spring.boot.config.processor)
 
-  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-  testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation(libs.bundles.spring.boot.test)
 }
 
 kotlin {
   compilerOptions {
-    freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    freeCompilerArgs.addAll(
+      "-Xjsr305=strict",
+      "-Xannotation-default-target=param-property",
+    )
   }
 }
 
