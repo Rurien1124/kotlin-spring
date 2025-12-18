2fced932d72df723b76eec63ae09bf1f15231f77
Rurien <a01092603909@gmail.com>
2025-12-18 13:16:54 +0900

    Add Google Gemini properties

---
+++ src/main/kotlin/io/github/rurien/common/property/GeminiProperties.kt
@@ -0,0 +1,8 @@
+package io.github.rurien.common.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "gemini.api")
+data class GeminiProperties(
+  val key: String,
+)
